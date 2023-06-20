2d060c8b1e6341032cf6ca999ce754097a330ae2
SujonHiro <sujonhowlader67@gmail.com>
2023-06-21 00:51:33 +0600

    update KEY

--- src/middleware/AuthVerifyMiddleware.js
+++ src/middleware/AuthVerifyMiddleware.js
@@ -3,7 +3,7 @@ const jwt=require('jsonwebtoken')
 module.exports=async(req,res,next)=>{
     try {
         const token = req.headers['token'];
-        const decoded = await jwt.verify(token, 'SecretKey123456789');
+        const decoded = await jwt.verify(token, 'secretKey1231321Sujon');
         const email = decoded['data'];
         req.headers.email = email;
         next();
